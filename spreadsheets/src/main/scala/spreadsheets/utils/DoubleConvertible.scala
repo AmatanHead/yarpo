7e24ceb98b5b4e5f2026da895e6fbf43a5b7ccdd
Vladimir Goncharov <amatanhead@yandex-team.ru>
2017-03-23 21:54:14 +0300

    Treat empty strings as zeros so that formulas act more like google spreadsheets

--- spreadsheets/src/main/scala/spreadsheets/utils/DoubleConvertible.scala
+++ spreadsheets/src/main/scala/spreadsheets/utils/DoubleConvertible.scala
@@ -4,6 +4,7 @@ object DoubleConvertible {
   def unapply(arg: Any): Option[(Double)] = arg match {
     case d: Double => Option(d)
     case d: Boolean => Option(if (d) 1 else 0)
+    case "" => Option(0)
     case s: String =>
       val res = try { Option(s.stripSuffix("%").toDouble) } catch { case _: java.lang.NumberFormatException => None }
       if (s.endsWith("%")) res.map(_ * 0.01) else res
